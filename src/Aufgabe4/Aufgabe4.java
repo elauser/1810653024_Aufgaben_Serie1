99991f00aa7b120eb9d03026f79180a1ede20c91
elauser <1810653024@stud.fh-kufstein.ac.at>
2018-11-08 09:42:12 +0100

    fifth commit

---
+++ src/Aufgabe4/Aufgabe4.java
@@ -0,0 +1,9 @@
+package Aufgabe4;
+
+public class Aufgabe4 {
+    public static void main(String[] args){
+        String name = "Matthias Oberleitner";
+        int alter = 21;
+        System.out.println(name + " ist " + alter + " Jahre alt");
+    }
+}
