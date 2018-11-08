c8dd19d78b5c162a3abeed1de52a409523e92883
elauser <1810653024@stud.fh-kufstein.ac.at>
2018-11-08 09:56:31 +0100

    Aufgabe 5

---
+++ src/Aufgabe4/Aufgabe5.java
@@ -0,0 +1,8 @@
+package Aufgabe4;
+
+public class Aufgabe5 {
+    public static void main(String[] args){
+        final String Aufgabe5
+    ;         // Strg + alt + f zeigt dass es zwar nicht falsch ist aber es hinter der variable sein soll
+    }
+}
