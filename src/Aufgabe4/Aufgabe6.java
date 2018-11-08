9c0caec374b86a2fedf9510bba6a24969dc9ee51
elauser <1810653024@stud.fh-kufstein.ac.at>
2018-11-08 10:20:12 +0100

    Aufgabe6

---
+++ src/Aufgabe4/Aufgabe6.java
@@ -0,0 +1,10 @@
+package Aufgabe4;
+
+public class Aufgabe6 {
+    public static void main(String[] args){
+        System.out.println("Java verwendet für Strings \"Doppelte Anführungszeichen\" und einzelne Anführungszeichen wie \'c\' um einzelne Zeichen zu definieren");
+        System.out.println("Durch \\ kann man auch den Backslash selbst beliegib oft ausgeben: \\\\\\\\");
+        System.out.println("Ich verwende den\b Backspace, die Newline\n und den \tTabulator");
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt die bestehende Zeile. \fist in der Konsole nicht bemerkbar, da es einen Seitenumbruch erzwingt.");
+    }
+}
